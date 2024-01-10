4a0568f8256300c1911247900e5c4b1f67943dd4
Respect-J <respect0170@gmail.com>
2024-01-10 19:17:16 +0500

    add post api for solves

--- index/solves/urls.py
+++ index/solves/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import solves
+from .views import solves, add_solve
 
 urlpatterns = [
-    path('get/<uuid:pk>/', solves)
+    path('get/<uuid:pk>/', solves),
+    path('post/', add_solve)
 
 ]
--- index/solves/views.py
+++ index/solves/views.py
@@ -1,24 +1,24 @@
+from rest_framework import status
 from rest_framework.response import Response
 from .models import Solves
 from tests.models import Tests
 from .serializers import SolveSerializers
 from rest_framework.decorators import api_view
 
-@api_view(['GET', 'Post'])
+@api_view(['GET'])
 def solves(request, pk):
     if request.method == 'GET':
         solve = Solves.objects.filter(user_id=pk).values_list('test', flat=True).distinct()
-        return Response(solve)
+        return Response(solve, status=status.HTTP_200_OK)
 
-    elif request.method == 'POST':
-        serializer = SnippetSerializer(data=request.data)
+
+
+@api_view(['POST'])
+def add_solve(request):
+
+    if request.method == 'POST':
+        serializer = SolveSerializers(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
-
-
